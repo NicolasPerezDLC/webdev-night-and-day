850c22d370998122186a1bf04fc4cf639d7046c3
Perez de las casas Nicolas <20265@columbushs.com>
2019-10-29 09:41:15 -0400

    a

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    $('#orb').click(function(){
+        if ($('body').hasClass('black')) {
+            $('body').removeClass('black').addClass('white');
+        }
+        else {
+            $('body').removeClass('white').addClass('black');
+        }    
+            if ($(this).hasClass('sun')) {
+                $(this).removeClass('sun').addClass('moon');
+            }
+            else {
+                $(this).removeClass('moon').addClass('sun');
+            }
+        if ($('#sky').hasClass('day')) {
+            $('#sky').removeClass('day').addClass('night');
+        }
+        else {
+            $('#sky').removeClass('night').addClass('day');
+        }
+        if ($('#moonspot1').hasClass('visible')) {
+            $('#moonspot1').removeClass('visible');
+        } else {
+            $('#moonspot1').addClass('visible');
+        }
+    });
+});
